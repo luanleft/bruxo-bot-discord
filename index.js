70697c7c124ad9130bdb4754fd748b500bced0b6
Luan <113206529+luanleft@users.noreply.github.com>
2026-04-24 15:27:13 -0300

    Implement CORS for specific origins
    Added CORS support for specific origins.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'https://seu-tumblr.tumblr.com']}));
 app.use(express.json());
 
 const ROLE_MAP = {
